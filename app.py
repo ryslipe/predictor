2688a9a6788144be8149a5d039b4511668d59981
ryslipe <125102205+ryslipe@users.noreply.github.com>
2024-06-25 10:49:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -35,10 +35,10 @@ df_table = df.copy()
 df_table['season'] = df_table['season'].astype(str)
 
 # upload our trained models so they don't take too long to run.
-qb_model_lasso = load('qb_lasso_model_new.pkl')
-qb_model_ridge = load('qb_ridge_model_new.pkl')
-qb_model_gb = load('qb_gb_model_new.pkl')
-qb_model_knn = load('qb_knn_model_new.pkl')
+qb_model_lasso = load('data/qb_lasso_model_new.pkl')
+qb_model_ridge = load('data/qb_ridge_model_new.pkl')
+qb_model_gb = load('data/qb_gb_model_new.pkl')
+qb_model_knn = load('data/qb_knn_model_new.pkl')
 
 with st.sidebar:
     selected = option_menu(
