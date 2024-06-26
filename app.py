2d3c506bbd91db8b18d558c5e9f57dc1795f43bc
ryslipe <125102205+ryslipe@users.noreply.github.com>
2024-06-25 20:09:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -184,7 +184,185 @@ if selected == 'Quarterbacks':
     X_train_qb = qb_train.drop(columns = ['player_id', 'player_display_name', 'fantasy_points_ppr'], axis = 1)
     y_train_qb = qb_train['fantasy_points_ppr'] 
     
+    ######################################################################################################################################################
+    # create our pipeline
+    pipelines = {
+        'knn': make_pipeline(StandardScaler(), KNeighborsRegressor()),
+        'rf' : make_pipeline(StandardScaler(), RandomForestRegressor()),
+        'gb' : make_pipeline(StandardScaler(), GradientBoostingRegressor()),
+        'ridge': make_pipeline(StandardScaler(), Ridge()),
+        'lasso': make_pipeline(StandardScaler(), Lasso())
+    }
     
+    # these will be our 5 models. It will fit to our training data and create a dictionary of each model. 
+    # try to make that into a function.
+    def model_creation(X, y):
+        dic = {}
+        for algo, pipeline in pipelines.items():
+            model = pipeline.fit(X, y)
+            dic[algo] = model
+        return dic
+    
+    
+    # call our function to fit the models 
+    qb_mods = model_creation(X_train_qb, y_train_qb)
+    
+    
+    # function to see our RMSE on full training data (without cross validation being used.)
+    # rmse of our models on full training data - don't really need this tbh
+    def full_train_rmse(model_dict, X, y):
+        '''A function to produce the RMSE on full training data without use of cross validation.'''
+        rmse_models = {}
+        for algo, model in model_dict.items():
+            # make a prediction on training data using each model
+            pred = model.predict(X)
+            # calculate mse
+            rmse = root_mean_squared_error(y, pred)
+            # calculate rmse
+            rmse_models[algo] = (rmse)
+        return rmse_models
+    
+    # call the function then print the results.
+    qb_train_rmse = full_train_rmse(qb_mods, X_train_qb, y_train_qb)
+    qb_train_rmse.items()
+    
+    # neat way to print the results
+    def printing_rmse(rmse_dict):
+        for k, v in rmse_dict.items():
+            st.write(k, ':',v)
+    
+    # call printing function with our dictionary
+    printing_rmse(qb_train_rmse)
+    
+    # bar graph of our rmse results
+    import matplotlib.pyplot as plt
+    # set up x and y values
+    x_val = ['knn', 'rf', 'gb', 'ridge', 'lasso']
+    y_val = list(qb_train_rmse.values())
+    
+    
+    # Graph the results 
+    def make_rmse_plot(rmse_dict, title, ylim):
+        x_val = ['knn', 'rf', 'gb', 'ridge', 'lasso']
+        y_val = list(rmse_dict.values())
+        # create the graph
+        fig_1, ax = plt.subplots()
+        ax.bar(x_val, y_val, color = ['Red', 'Green', 'Black', 'Orange', 'Blue'])
+        ax.set_title(title, fontsize = 24)
+        ax.set_ylabel('rmse', fontsize = 14)
+        ax.set_ylim(ylim)
+        return fig_1
+    
+    # call the plotting function
+    fig_1 = make_rmse_plot(qb_train_rmse, 'RMSE Plot without Cross Validation', [0, 9])
+    if st.button('Generate RMSE Report'):
+        st.pyplot(fig_1)
+        
+    st.write('The results of the RMSE show that random forest is the best model but there is potenial for overfitting.')
+    
+    
+    
+    
+    #################################################################################
+    # Next, we get into the grid search. First, create the grid.
+    # grid 
+    grid = {
+        'knn': {
+            'kneighborsregressor__n_neighbors': [30, 35, 40, 45, 50],
+        },
+        'rf': {
+            'randomforestregressor__n_estimators': [100, 150, 200],
+            'randomforestregressor__max_features': [2, 3, 4, 5],
+            'randomforestregressor__max_depth': [2, 3, 4]
+        },
+        'gb':{
+            'gradientboostingregressor__n_estimators': [100, 200, 300],
+            'gradientboostingregressor__max_features': [2, 3, 4, 5],
+            'gradientboostingregressor__learning_rate': [0.001, 0.01, 0.1, 0.5],
+            'gradientboostingregressor__max_depth': [2, 3, 4]
+        },
+        'ridge':{
+            'ridge__alpha': [20, 25, 30, 35],
+        },
+        'lasso': {
+            'lasso__alpha': [0.25, 0.5, 0.75, 1]
+        }
+    }
+    
+    
+    # fit the grid search for each model
+    def grid_search_models(position_model_dict, X, y):
+        '''Function to perform gridsearch on all of our models.'''
+        # empty dictionary to start
+        searched_models = {}
+        for algo, model in position_model_dict.items():
+            # keep track of where its at
+            print(f'Training the {algo} model')
+            # find best parameters
+            search = GridSearchCV(model, grid[algo], scoring = 'neg_mean_squared_error', cv = 5, return_train_score= True)
+            # fit those best parameters on training data
+            search.fit(X, y)
+            # add to dictionary
+            searched_models[algo] = search
+        return searched_models
+    
+    # call the function
+    qb_mods_cv = grid_search_models(qb_mods, X_train_qb, y_train_qb)
+
+    
+    ### ANOTHER FUNCTION (NUMBER IT)
+    def min_rmse(results):
+        '''Function to return the lowest RMSE score of each model.'''
+        # set up list for each cv score
+        scores = []
+        # find the 'neg_mean_squared_error' for each cv
+        for mean_score in results['mean_test_score']:
+            # get rmse by taking sqrt of neg mean_score
+                scores.append(np.sqrt(-mean_score))
+        # return lowest rmse
+        return min(scores)
+    
+    ### FUNCTION FOR ALL CV RMSE
+    def cv_rmse(searched_model_dict):
+        '''A function to get the lowest RMSE of our models.'''
+        all_rmse = {}
+        for algo, score in searched_model_dict.items():
+            result = min_rmse(searched_model_dict[algo].cv_results_)
+            all_rmse[algo] = result
+        return all_rmse
+    
+    # call function to create dictionary of all lowest RMSE of cross val models
+    qb_searched_rmse = cv_rmse(qb_mods_cv)
+    
+    # call rmse printing function for neatly printed data.
+    printing_rmse(qb_searched_rmse)
+    
+    # plot after CV
+    title = 'RMSE Plot After Cross Validation'
+    make_rmse_plot(qb_searched_rmse, title)
+    
+    
+    # random forest model shows us feature importances
+    feature_importances_qb = qb_mods_cv['rf'].best_estimator_._final_estimator.feature_importances_
+    
+    # set up the features for feature importance
+    attributes = list(X_train_qb.columns)
+    # importance of our features
+    sorted(zip(feature_importances_qb, attributes), reverse = True)
+    
+    # make a function that gets feature importances and prints it 
+    def feature_importances(rf_model, X):
+        '''Print the feature importances.'''
+        feature_importances = rf_model.best_estimator_._final_estimator.feature_importances_
+        # get columns to match feature importances
+        attributes = list(X.columns)
+        # importances
+        importances = sorted(zip(feature_importances, attributes), reverse = True)
+        return importances
+    
+    # call the function for our importances
+    importances = feature_importances(qb_mods_cv['rf'], X_train_qb)
+    ######################################################################################################################################################
     # create our pipeline
     pipelines = {
         'knn': make_pipeline(StandardScaler(), KNeighborsRegressor()),
